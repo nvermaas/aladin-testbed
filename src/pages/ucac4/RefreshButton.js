94a9e998c7bc15ae61f5f6b7ca0af19f27580a82
Nico Vermaas <vermaas@astron.nl>
2022-08-20 17:23:51 +0200

    add reload data when position changed

---
+++ src/pages/ucac4/RefreshButton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import { useGlobalReducer } from '../../contexts/GlobalContext';
+import { RELOAD_UCAC4 } from '../../contexts/GlobalStateReducer'
+
+import { getReloadIcon } from '../../utils/styling'
+
+export default function RefreshButton(props) {
+    const [ my_state , my_dispatch] = useGlobalReducer()
+
+    const handleClick = () => {
+        my_dispatch({type: RELOAD_UCAC4, reload_ucac4: !my_state.reload_ucac4})
+    }
+
+    return <Button variant="outline-primary" onClick={() => handleClick()}>{getReloadIcon()}&nbsp;Refresh</Button>
+}

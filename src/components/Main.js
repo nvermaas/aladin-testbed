5b702a4bef2bf8d2b134f4f8f7f5096ff7e33cc3
vermaas <vermaas@astron.nl>
2022-10-21 19:03:23 +0200

    update timestamp

--- src/components/Main.js
+++ src/components/Main.js
@@ -51,7 +51,7 @@ export default function Main() {
                 </Switch>
             </div>
             <footer>
-                <small> (C) 2021 - Nico Vermaas - version 1.0.0 - 15 oct 2022 - 16:00</small>
+                <small> (C) 2021 - Nico Vermaas - version 1.0.0 - 21 oct 2022 - 19:00</small>
             </footer>
         </Router>
 
